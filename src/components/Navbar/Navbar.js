1c4592a7473f040e786377a8278503fed284fb6d
Marcin Treder <marcin@uxpin.com>
2020-01-11 16:41:28 -0800

    HTML fix in navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -40,7 +40,7 @@ const Navbar = class extends React.Component {
       >
         <div className="container">
           <div className="navbar-brand">
-            <Link to="/" className="navbar-item" title="Logo">
+            <Link to="/" className="navbar-item navbar-logo" title="Logo">
               <img src={logo} alt="Relvel" style={{ width: "88px" }} />
             </Link>
           </div>
