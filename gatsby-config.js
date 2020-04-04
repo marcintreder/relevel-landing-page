f86c10e3839500feb993f27c5f216c4fd91b79e5
Marcin Treder <marcin@uxpin.com>
2020-04-04 14:30:44 -0700

    fullstory and ga fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,20 +9,18 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        // The property ID; the tracking code won't be generated without it
-        trackingId: "UA-162840050-1",
+        trackingId: 'UA-162840050-1',
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: true,
         // Setting this parameter is optional
         anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ["/admin/**"],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
+        exclude: ['/admin/**'],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-fullstory`,
+      options: {
+        fs_org: 'T7A01',
       },
     },
     'gatsby-plugin-react-helmet',
