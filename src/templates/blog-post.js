79337048c339621217c95ad59f50261f18a2ee97
Marcin Treder <marcin@uxpin.com>
2020-04-04 19:31:22 -0700

    Blog post template corrected

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,13 +1,24 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { kebabCase } from 'lodash'
-import Helmet from 'react-helmet'
-import { graphql, Link } from 'gatsby'
-import { DiscussionEmbed } from 'disqus-react'
-import Layout from '../components/Layout'
-import Content, { HTMLContent } from '../components/Content'
-import Breadcrumb from "../components/Breadcrumb/Breadcrumb"
-import useSiteMetadata from '../hooks/siteMetedata';
+import React from "react";
+import PropTypes from "prop-types";
+import { kebabCase } from "lodash";
+import Helmet from "react-helmet";
+import { graphql, Link } from "gatsby";
+import { DiscussionEmbed } from "disqus-react";
+import Layout from "../components/Layout";
+import Content, { HTMLContent } from "../components/Content";
+import Breadcrumb from "../components/Breadcrumb/Breadcrumb";
+import useSiteMetadata from "../hooks/siteMetedata";
+
+import {
+  FacebookShareButton,
+  FacebookIcon,
+  TwitterShareButton,
+  TwitterIcon,
+  WhatsappShareButton,
+  WhatsappIcon,
+  LinkedinShareButton,
+  LinkedinIcon
+} from "react-share";
 
 export const BlogPostTemplate = ({
   content,
@@ -19,57 +30,74 @@ export const BlogPostTemplate = ({
   id,
   location
 }) => {
-  const PostContent = contentComponent || Content
-  
+  const PostContent = contentComponent || Content;
+
   /* disqus integration */
   const { siteURL } = useSiteMetadata();
-  const pageURL = `${siteURL}${location.pathname}`
+  const pageURL = `${siteURL}${location.pathname}`;
   let disqusConfig = {
     url: pageURL,
     identifier: `relevel-1${id}`,
-    title: title,
-  }
+    title: title
+  };
 
   return (
     <section className="section">
-      {helmet || ''}
+      {helmet || ""}
       <div className="container content blogpost-container">
-            <h1 className="blogpost-header">
-              {title}
-            </h1>
-            <Breadcrumb secondPage="blog" currentPage={title} />
-            <p>{description}</p>
-            <PostContent content={content} className="blogpost-content" />
-            {tags && tags.length ? (
-              <div className="blogpost-tags">
-                <h2>Read More</h2>
-                <ul className="taglist">
-                  {tags.map(tag => (
-                    <li key={tag + `tag`}>
-                      <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            ) : null}
+        <article className="container blogpost-text">
+          <h1 className="blogpost-header">{title}</h1>
+          <Breadcrumb secondPage="blog" currentPage={title} />
+          <p>{description}</p>
+          <PostContent content={content} className="blogpost-content" />
+        </article>
+        <aside className="blogpost-aside">
+          <h2>Share</h2>
+          <div>
+            <FacebookShareButton url={pageURL}>
+              <FacebookIcon size={32} />
+            </FacebookShareButton>
+            <TwitterShareButton url={pageURL}>
+              <TwitterIcon size={32} />
+            </TwitterShareButton>
+            <LinkedinShareButton url={pageURL}>
+              <LinkedinIcon size={32} />
+            </LinkedinShareButton>
+            <WhatsappShareButton url={pageURL}>
+              <WhatsappIcon size={32} />
+            </WhatsappShareButton>
+          </div>
+          {tags && tags.length ? (
+            <div className="blogpost-tags">
+              <h2>Read More</h2>
+              <ul className="blogpost-taglist">
+                {tags.map(tag => (
+                  <li key={tag + `tag`}>
+                    <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ) : null}
+        </aside>
       </div>
-      <div className="container">
+      <div className="container blog-discussion">
         <DiscussionEmbed config={disqusConfig} shortname="relevel-1" />
       </div>
     </section>
-  )
-}
+  );
+};
 
 BlogPostTemplate.propTypes = {
   content: PropTypes.node.isRequired,
   contentComponent: PropTypes.func,
   description: PropTypes.string,
   title: PropTypes.string,
-  helmet: PropTypes.object,
-}
+  helmet: PropTypes.object
+};
 
 const BlogPost = ({ data, location }) => {
-  const { markdownRemark: post } = data
+  const { markdownRemark: post } = data;
   return (
     <Layout>
       <BlogPostTemplate
@@ -91,16 +119,16 @@ const BlogPost = ({ data, location }) => {
         id={post.id}
       />
     </Layout>
-  )
-}
+  );
+};
 
 BlogPost.propTypes = {
   data: PropTypes.shape({
-    markdownRemark: PropTypes.object,
-  }),
-}
+    markdownRemark: PropTypes.object
+  })
+};
 
-export default BlogPost
+export default BlogPost;
 
 export const pageQuery = graphql`
   query BlogPostByID($id: String!) {
@@ -115,4 +143,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
